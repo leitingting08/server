4bfc2caac21cb956bccf2656861703365dc00992
leitingting08 <leitingting08@gmail.com>
2018-04-06 17:26:15 +0800

    auction_server

--- build/auction_server.js
+++ build/auction_server.js
@@ -22,11 +22,35 @@ var products = [
     new Product(5, "第5个商品", 5.99, 3.5, "这是第5个商品", ["电子产品", "硬件设备"]),
     new Product(6, "第6个商品", 6.99, 4.5, "这是第6个商品", ["图书"]),
 ];
-app.get('/', function (req, res) {
+var Comment = /** @class */ (function () {
+    function Comment(id, productId, timestamp, user, rating, content) {
+        this.id = id;
+        this.productId = productId;
+        this.timestamp = timestamp;
+        this.user = user;
+        this.rating = rating;
+        this.content = content;
+    }
+    return Comment;
+}());
+exports.Comment = Comment;
+var comments = [
+    new Comment(1, 1, "2018-02-02 22:22:22", "张三", 3, "东西不错"),
+    new Comment(2, 1, "2018-01-02 16:22:22", "张三", 4, "东西挺不错"),
+    new Comment(3, 1, "2018-03-02 21:22:22", "张三", 2, "东西还不错"),
+    new Comment(4, 2, "2018-04-02 23:22:22", "张三", 4, "东西非常不错")
+];
+app.get('/api', function (req, res) {
     res.send("hello express");
 });
-app.get('/products', function (req, res) {
+app.get('/api/products', function (req, res) {
     res.json(products);
+});
+app.get('/api/product/:id', function (req, res) {
+    res.json(products.find(function (product) { return product.id == req.params.id; }));
+});
+app.get('/api/product/:id/comments', function (req, res) {
+    res.json(comments.filter(function (comment) { return comment.productId == req.params.id; }));
 });
 var server = app.listen(8000, "localhost", function () {
     console.log("服务器已启动，地址是：http://localhost:8000");
